5d0a5068f9eb5675afcdde10995adc7102d16e40
Renan <renanmartim850@gmail.com>
2023-07-24 23:29:17 -0300

    Add Projeto

---
+++ Project-Cloud/src/test/java/com/cloud_project/Project/Cloud/ProjectCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_project.Project.Cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
